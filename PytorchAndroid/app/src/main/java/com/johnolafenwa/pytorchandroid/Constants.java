53f37937b5ad8b1ebece7a60c724156dff618df4
nacoline <792195188@qq.com>
2024-10-23 15:52:35 +0800

    Initial commit

---
+++ PytorchAndroid/app/src/main/java/com/johnolafenwa/pytorchandroid/Constants.java
@@ -0,0 +1,19 @@
+package com.johnolafenwa.pytorchandroid;
+
+public class Constants {
+
+    public static String[] IMAGENET_CLASSES = new String[]{
+            "Bread",
+            "Dairy product",
+            "Dessert",
+            "Egg",
+            "Fried food",
+            "Meat",
+            "Noodles/Pasta",
+            "Rice",
+            "Seafood",
+            "Soup",
+            "Vegetable/ Fruit"
+    };
+}
+
